e8dfc1d5c122ffb13c0ddfb25e4afcb71a867538
Rajesh Rout <55666427+rajeshrout97@users.noreply.github.com>
2022-01-12 17:21:21 +0530

    Added secret key and access key

--- lambda_function.py
+++ lambda_function.py
@@ -10,22 +10,22 @@ import io
 from io import StringIO
 def lambda_handler(event, context):
     s3_file_key = event['Records'][0]['s3']['object']['key'];
-    bucket = 'sourcebucketdemo2';
-    s3 = boto3.client('s3', aws_access_key_id='AKIAZRRIY4NWWO6V7QBS',  aws_secret_access_key='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')
+    bucket = 'staticfilesott';
+    s3 = boto3.client('s3', aws_access_key_id='AKIAVRBYUIFU5V2E7M7I',  aws_secret_access_key='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')
     obj = s3.get_object(Bucket=gitbucket, Key=s3_file_key)
     initial_df = pd.read_csv(io.BytesIO(obj['Body'].read()));
 
     service_name = 's3'
     region_name = 'ap-south-1'
-    aws_access_key_id = 'AKIAZRRIY4NWWO6V7QBS'
-    aws_secret_access_key = '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'
+    aws_access_key_id = 'AKIAVRBYUIFU5V2E7M7I'
+    aws_secret_access_key = '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'
     s3_resource = boto3.resource(
         service_name=service_name,
         region_name=region_name,
         aws_access_key_id=aws_access_key_id,
         aws_secret_access_key=aws_secret_access_key
     )
-    bucket='targetbucketdemo02';
+    bucket='targetbucketcleaned';
     df = initial_df[(initial_df.type == "Movie")];
     df1 = df.loc[:, ~df.columns.isin(['date_added', 'description', 'duration'])];
     csv_buffer = StringIO()
