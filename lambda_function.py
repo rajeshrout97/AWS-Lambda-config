27f0fde3ee75bdf97b50d0736dff7157523168c2
Rajesh Rout <55666427+rajeshrout97@users.noreply.github.com>
2022-01-12 17:43:36 +0530

    rectify the gitbucket code with bucket name

--- lambda_function.py
+++ lambda_function.py
@@ -12,7 +12,7 @@ def lambda_handler(event, context):
     s3_file_key = event['Records'][0]['s3']['object']['key'];
     bucket = 'staticfilesott';
     s3 = boto3.client('s3', aws_access_key_id='AKIAVRBYUIFU5V2E7M7I',  aws_secret_access_key='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')
-    obj = s3.get_object(Bucket=gitbucket, Key=s3_file_key)
+    obj = s3.get_object(Bucket=bucket, Key=s3_file_key)
     initial_df = pd.read_csv(io.BytesIO(obj['Body'].read()));
 
     service_name = 's3'
